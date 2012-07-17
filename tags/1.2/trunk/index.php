f03b84d757f370ee7f63d6c4814b5fa1ec66caaf
EnigmaWeb <EnigmaWeb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-17 02:42:15 +0000

    Tagging Version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/html5-jquery-audio-player@573369 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.2/trunk/index.php
@@ -0,0 +1,229 @@
+<?php
+
+/*
+Plugin Name: HTML5 jQuery Audio Player
+Plugin URI: http://wordpress.org/extend/plugins/html5-jquery-audio-player/
+Description: The trendiest audio player plugin for WordPress. Works on iPhone/iPad and other mobile devices. Insert with shortcode [hmp_player]
+Author: Enigma Digital
+Version: 1.2
+Author URI: http://enigmaweb.com.au
+*/
+
+
+//Database table versions
+global $hmp_player_db_table_version;
+$hmp_player_db_table_version = "1.0";
+
+//Create database tables needed by the DiveBook widget
+function hmp_db_create () {
+    hmp_create_table_player();
+}
+
+//Create dive table
+function hmp_create_table_player(){
+    //Get the table name with the WP database prefix
+    global $wpdb;
+    $table_name = $wpdb->prefix . "hmp_playlist";
+    global $hmp_player_db_table_version;
+    $installed_ver = get_option( "hmp_player_db_table_version" );
+     //Check if the table already exists and if the table is up to date, if not create it
+    if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name
+            ||  $installed_ver != $hmp_player_db_table_version ) {
+        $sql = "CREATE TABLE " . $table_name . " (
+              `id` INT( 9 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+              `mp3` VARCHAR( 100 ) NOT NULL,
+              `ogg` VARCHAR( 100 ) NOT NULL,
+              `rating` VARCHAR( 10 ) NOT NULL,
+              `title` VARCHAR( 50 ) NOT NULL ,
+              `buy` VARCHAR( 100 ) NOT NULL,
+			  `price` VARCHAR( 50 ) NOT NULL,
+			  `cover` VARCHAR( 100 ) NOT NULL,
+			  `duration` VARCHAR( 20 ) NOT NULL,
+			  `artist` VARCHAR( 50 ) NOT NULL,
+              UNIQUE KEY id (id)
+            );";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+        update_option( "hmp_player_db_table_version", $hmp_player_db_table_version );
+}
+    //Add database table versions to options
+    add_option("hmp_player_db_table_version", $hmp_player_db_table_version);
+}
+
+register_activation_hook( __FILE__, 'hmp_db_create' );
+
+
+
+
+
+
+
+
+
+add_action( 'admin_menu', 'my_plugin_menu' );
+
+
+
+
+function my_plugin_menu() {
+	add_menu_page( 'HTML5 MP3 Player', 'HTML5 Player', 'manage_options', 'hmp-options', 'wp_hmp_options',plugin_dir_url( __FILE__ )."/music-beam.png" );
+	add_submenu_page('hmp-options','','','manage_options','hmp-options','wp_hmp_options');
+	add_submenu_page('hmp-options','Display Settings','Display Settings','manage_options','display_settings','wp_hmp_options');
+	add_submenu_page( 'hmp-options', 'Manage Playlists', 'Playlists', 'manage_options', 'hmp_palylist', 'wp_hmp_playlist' );
+	
+}
+
+
+add_action( 'admin_init', 'register_mysettings' );
+
+function register_mysettings() {
+	register_setting( 'baw-settings-group', 'buy_text' );
+	register_setting( 'baw-settings-group', 'color' );
+	register_setting( 'baw-settings-group', 'showlist' );
+	register_setting( 'baw-settings-group', 'showbuy' );
+	register_setting( 'baw-settings-group', 'hmp_description' );
+	register_setting( 'baw-settings-group', 'currency' );
+	register_setting( 'baw-settings-group', 'tracks' );
+	register_setting( 'baw-settings-group', 'tcolor' );
+}
+
+
+
+
+
+
+function wp_hmp_options() {
+
+include 'player/settings.php';
+
+ }
+
+
+
+function wp_hmp_playlist(){
+		
+include('playlist/add_playlist.php');
+		
+}
+
+function wp_hmp_player(){
+	
+	
+	if(get_option('showbuy')==1){ 
+		$bt		=	get_option('buy_text');
+	}else{
+		$bt	=	'';
+	}
+	$desc	=	get_option('hmp_description');
+	$sb		=	get_option('showbuy');
+	$nt		=	get_option('tracks');
+	if(empty($nt)){
+		$nt		=	1;
+	}
+	
+	$cr		=	get_option('currency');
+	$sl		=	get_option('showlist');
+	$cl		=	get_option('color');
+	/*if(empty($cl)){
+		$cl		=	'currentcolor';
+	}*/
+	$tc		=	get_option('tcolor'); 
+	if(empty($tc)){
+		$tc		=	'#cccccc';
+	}	
+	
+	if($sb==0){ ?> <style type="text/css">.buy{ display:none !important;} .rating{ right:10px !important;}</style><?php } 
+	if(!empty($cl)){ ?><style type="text/css"> .ttw-music-player{ background:<?php echo $cl; ?>; !important;}   </style><?php }
+	if(!empty($tc)){ ?><style type="text/css"> .ttw-music-player .tracklist, .ttw-music-player .buy, .ttw-music-player .description, .ttw-music-player .player .title, .ttw-music-player .artist, .ttw-music-player .artist-outer{ color:<?php echo $tc; ?>; !important;}   </style><?php }
+	if($tc=='black'){ ?><style type="text/css">.ttw-music-player .player .title, .ttw-music-player .description, .ttw-music-player .tracklist li{ text-shadow:none !important;}</style><?php } 
+	if($sl==0){ ?> <style type="text/css"> .tracklist{ display:none !important;}   </style> <?php }
+	
+	  
+	$pluginurl	=	plugin_dir_url( __FILE__ );
+?>
+	<link href="<?php echo $pluginurl ; ?>includes/css/style.css" type="text/css" rel="stylesheet" media="screen" />
+    <script type="text/javascript" src="<?php echo $pluginurl ; ?>player/js/jquery-1.7.2.min.js"></script>
+    <script type="text/javascript" src="<?php echo $pluginurl ; ?>includes/jquery-jplayer/jquery.jplayer.js"></script>
+    <script type="text/javascript" src="<?php echo $pluginurl ; ?>includes/ttw-music-player-min.js"></script>
+   
+     <script type="text/javascript">
+	
+	
+	var myPlaylist = [
+
+<?php  
+
+
+
+	global $wpdb;
+	$table		=	$wpdb->prefix.'hmp_playlist';	
+	$lsql		=	"SELECT * FROM $table";
+
+	$songs 	= 	$wpdb->get_results( $lsql  );
+	
+	
+
+//$artists		=	array('sajid hussain','tasbeel hussain','omer','ali');
+
+if(!empty($songs)):
+foreach($songs as $song): ?>
+    {
+        mp3:'<?php echo $song->mp3; ?>',
+        oga:'<?php echo $song->ogg; ?>',
+        title:'<?php echo $song->title; ?>',
+        artist:'<?php echo $song->artist; ?>',
+        rating:<?php echo $song->rating; ?>,
+        buy:'<?php echo $song->buy; ?>',
+        price:'<?php echo $song->price; ?>',
+        duration:'<?php echo $song->duration; ?>',
+        cover:'<?php echo $song->cover; ?>'
+    }, <?php endforeach ?>
+	<?php else: ?>
+	
+	{
+        mp3:'<?php echo $pluginurl; ?>player/mix/1.mp3',
+        oga:'<?php echo $pluginurl; ?>player/mix/1.ogg',
+        title:'Sample Track',
+        artist:'Sample',
+        rating:4,
+        buy:'#',
+        price:'1.00',
+        duration:'4:50',
+        cover:'<?php echo $pluginurl; ?>player/mix/1.png'
+    }
+	
+	
+	<?php endif; ?>
+];
+
+	
+	
+	
+        $(document).ready(function(){
+           
+
+            $('#myplayer').ttwMusicPlayer(myPlaylist, {
+				
+				currencySymbol:'<?php echo $cr; ?>',
+        		description:'<?php echo $desc; ?>',
+				buyText:'<?php echo $bt; ?>',
+        		tracksToShow:<?php echo $nt; ?>,
+        		
+        
+				
+				});
+        });
+    </script>
+    <div id="myplayer"></div>
+<?php
+
+}
+add_shortcode('hmp_player','wp_hmp_player');
+
+
+function my_admin_scripts() {
+    wp_enqueue_style( 'farbtastic' );
+    wp_enqueue_script( 'farbtastic' );
+    wp_enqueue_script( 'my-theme-options', get_template_directory_uri() . '/js/theme-options.js', array( 'farbtastic', 'jquery' ) );
+}
---
+++ tags/1.2/trunk/readme.txt
@@ -0,0 +1,98 @@
+=== HTML5 jQuery Audio Player ===
+Contributors: EnigmaWeb, Base29
+Donate link: http://enigmaweb.com.au
+Tags: mp3 player, audio player, music player, ogg player, HTML5 audio player, mp3, podcast, jquery player
+Requires at least: 3.1
+Tested up to: 3.4.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Finally, a trendy looking audio player plugin. Works on all browsers including iPhone/iPad.
+
+== Description ==
+
+This trendy looking music player lets you add a single audio track or a full playlist to your WordPress site using shortcode. You can customise the colours of the player, and also display ratings, album cover art, and buy/download link if needed. This audio player is different from others on offer because it works on all major browsers, both PC and Mac, and on mobile devices including iPhone/iPad. Plus it looks really cool!
+
+= Key Features =
+
+*	Supports mp3 and ogg file formats
+*   Attractive design with customisable colours
+*	HTML5 based player with Flash backup 
+*   Works in all major browsers - IE7, IE8, IE9, Safari, Firefox, Chrome
+*   Works on mobile devices including iPhone/iPad
+*   Can enable Buy or Download tracks button
+*	Add the player to any post/page using shortcode `[hmp_player]`
+
+= Demo =
+
+[Click here for demo](http://www.galaxybrass.com.au/audio-demo/)
+
+= Credits =
+
+This is a WordPress version of the player created by Saleem over at [Codebase Hero](http://www.codebasehero.com/2011/06/html-music-player/) with thanks also to [Orman Clark](http://www.premiumpixels.com/freebies/compact-music-player-psd/) for the original PSD.
+
+== Installation ==
+
+1. Upload the `html5-jquery-audio` folder to the `/wp-content/plugins/` directory
+1. Activate the HTML5 jQuery Audio Player plugin through the 'Plugins' menu in WordPress
+1. Configure the plugin by going to the `HTML5 Audio Player` menu that appears in your admin menu
+1. Add the player to any post/page using shortcode `[hmp_player]`
+
+== Frequently Asked Questions ==
+
+= I'm having problems adding mp3 files - they won't play. =
+
+The plugin supports mp3 and ogg files. You need to upload both an mp3 and ogg version of each track in the playlist. Please also check your files are encoded correctly, and confirm that your file paths are correct. jPlayer sometimes has problems with relative urls so make sure you're using the absolute paths.
+
+= How can I convert my files to mp3 and ogg? =
+
+There are heaps of free conversion tools available - run a search. Personally, I use [Goldwave.](http://www.goldwave.com/)
+
+= Does this work on all browsers? =
+
+Yes. Its designed to work on all major browsers, both PC & Mac, and on mobile devices including iPhone/iPad.
+
+= Is there a way to easliy replace the 'Buy' button with 'Download' button? =
+
+Yes! Set the buy text option to ‘Download’, leave currency field & song price field blank, and set the buy link option on each song to the url of the mp3 or the script that initiates the download.
+
+= Can I use this on a non-WordPress site? =
+
+Yep, sure can. This plugin is just a WordPress version of HTML5 Music Player by Saleem over at [Codebase Hero](http://www.codebasehero.com/2011/06/html-music-player/)
+
+= Can you help me add volume knob, mute function, remove the star rating, add my company’s logo to it, build in functionality to play videos, add a shopping cart module with credit card and PayPal checkout, add a library of 1000 albums including rights for all the music and hand over the code to me for free? =
+
+No. Sorry.
+
+= Where can I get support for this plugin? =
+
+If you've tried all the obvious stuff and it's still not working please request support via the forum.
+
+
+== Screenshots ==
+
+1. An example of the inserted player
+2. The display settings screen in WP-Admin
+
+== Changelog ==
+
+= 1.2 =
+* Fixed a filepath bug - some users were unable to update/delete tracks from playlist, this is now fixed.
+
+= 1.1 =
+* Added sidebar with support forum, donate and wordpress.org links
+
+= 1.0 =
+* Initial release
+
+== Upgrade Notice ==
+
+= 1.2 =
+* Fixed a filepath bug - some users were unable to update/delete tracks from playlist, this is now fixed.
+
+= 1.1 =
+* Added sidebar with support forum, donate and wordpress.org links
+
+= 1.0 =
+* Initial release
